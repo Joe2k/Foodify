3f97413a979b325327c72f0edba97db6dc50510c
Jonathan <jonathansamuel2k@gmail.com>
2020-08-29 15:24:37 +0530

    login page

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb+srv://admin-joe:"+process.env.DB_PASS+"@cluster0.hbzmq.mongodb.net/saleDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.get("/",function (req,res) {
+    res.render("login");
+});
+
 let PORT=process.env.PORT || 3000;
 app.listen(PORT, function() {
     console.log("Server started on port 3000");
