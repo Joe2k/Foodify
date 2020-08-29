8b5a6f92e19a961ab89f20be2f8cdb494f57065e
Jonathan <jonathansamuel2k@gmail.com>
2020-08-29 14:58:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-joe:"+process.env.DB_PASS+"@cluster0.hbzmq.mongodb.net/saleDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+let PORT=process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("Server started on port 3000");
+});
